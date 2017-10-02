ec2ec8dba6076c5b90c047fe3f12e32bfe7c981e
mirek_stasiak <m.stasiak@mdbootstrap.com>
2017-10-02 10:09:39 +0200

    add waves effect to img card

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Waves from './Waves';
+
 
 class CardImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cursorPos: {}
+    }
+  }
+
+  handleClick(e){
+    // Get Cursor Position
+    let cursorPos = {
+      top: e.clientY,
+      left: e.clientX,
+      time: Date.now()
+    }
+    this.setState({ cursorPos: cursorPos })
+  }
 
 	render() {
  
@@ -13,7 +31,16 @@ class CardImage extends Component {
     } = this.props;
 
 		return (
-      <Tag {...attributes} className={className} />
+      <div className="Ripple-parent">
+        <Tag 
+          {...attributes} 
+          className={className} 
+          onMouseDown={ this.handleClick.bind(this) }
+          onTouchStart={ this.handleClick.bind(this) }
+        >
+        </Tag>
+        <Waves cursorPos={ this.state.cursorPos } />
+      </div>
 		);
 	}
 }
