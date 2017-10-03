efca69242e195ccb0cbac8f434f525f94d02923f
Mattonit <b.malanowski@mdbootstrap.com>
2017-10-03 12:59:46 +0200

    updated router to load pages dynamically

--- docs/App.js
+++ docs/App.js
@@ -1,35 +1,44 @@
 import React, { Component } from 'react';
-import { Button, Navbar, NavbarBrand, NavbarNav, NavbarToggler, NavItem, NavLink } from 'mdbreact';
+import { Button, Navbar, NavbarBrand, NavbarNav, NavbarToggler, NavItem} from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const NavLink = require('react-router-dom').NavLink;
+
 import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
-      <div className="flyout">
-        <Navbar color="indigo" dark expand="md" fixed="top" scrolling>
-          <NavbarBrand href="/">MDB React</NavbarBrand>
-          <NavbarToggler />
-            <div className="collapse navbar-collapse" id="reactNavbar">
+      <Router>
+        <div className="flyout">
+          <Navbar color="indigo" dark expand="md" fixed="top" scrolling>
+            <NavbarBrand href="/">MDB React</NavbarBrand>
+            <NavbarToggler />
+              <div className="collapse navbar-collapse" id="reactNavbar">
 
-              <NavbarNav className="ml-auto">
-                <NavItem>
-                  <NavLink href="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/css">CSS</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/components">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/javascript">JavaScript</NavLink>
-                </NavItem>
-              </NavbarNav>
+                <NavbarNav className="ml-auto">
+                  <NavItem>
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/css">CSS</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/components">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to="/javascript">JavaScript</NavLink>
+                  </NavItem>
+                </NavbarNav>
 
-            </div>
-        </Navbar>
-        <Routes />
-      </div>
+              </div>
+          </Navbar>
+
+          <main style={{'margin-top': '4rem'}}>
+            <Routes />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
