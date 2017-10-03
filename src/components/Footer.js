a0b1cb81d305d24c12329767c4e32695ea85ffb1
Mattonit <b.malanowski@mdbootstrap.com>
2017-10-03 14:05:41 +0200

    allow to set the color for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ class Footer extends Component {
 	render() {
  
     const {
+      color,
       children,
       className,
       tag: Tag,
@@ -15,6 +16,7 @@ class Footer extends Component {
 
     const classes = classNames(
       'page-footer',
+      color ? color : '',
       className,
     );
 
@@ -27,6 +29,7 @@ class Footer extends Component {
 }
 
 Footer.propTypes = {
+  color: PropTypes.string,
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   className: PropTypes.string,
   children: PropTypes.node
