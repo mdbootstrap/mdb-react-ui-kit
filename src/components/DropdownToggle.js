035248d6a8bf1d17f3feeea674dbf6adb8d183c1
mirek_stasiak <m.stasiak@mdbootstrap.com>
2017-10-04 11:41:13 +0200

    add a DropdownToogle

---
+++ src/components/DropdownToggle.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Target } from 'react-popper';
+import { Button } from 'mdbreact';
+
+class DropdownToggle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    if (this.props.disabled) {
+      e.preventDefault();
+      return;
+    }
+
+    if (this.props.nav && !this.props.tag) {
+      e.preventDefault();
+    }
+
+    if (this.props.onClick) {
+      this.props.onClick(e);
+    }
+
+    this.context.toggle(e);
+  }
+
+  render() {
+    const { className, color, caret, split, nav, tag, ...props } = this.props;
+    const ariaLabel = props['aria-label'] || 'Toggle Dropdown';
+    const classes = classNames(
+      {
+        'dropdown-toggle': caret || split,
+        'dropdown-toggle-split': split,
+        'nav-link': nav
+      },
+      className
+    );
+    const children = props.children || <span className="sr-only">{ariaLabel}</span>;
+
+    let Tag;
+
+    if (nav && !tag) {
+      Tag = 'a';
+      props.href = '#';
+    } else if (!tag) {
+      Tag = Button;
+      props.color = color;
+    } else {
+      Tag = tag;
+    }
+
+    return (
+      <Target
+        {...props}
+        className={classes}
+        component={Tag}
+        onClick={this.onClick}
+        aria-expanded={this.context.isOpen}
+        children={children}
+      />
+    );
+  }
+}
+
+DropdownToggle.propTypes = {
+  caret: PropTypes.bool,
+  color: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  'aria-haspopup': PropTypes.bool,
+  split: PropTypes.bool,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  nav: PropTypes.bool,
+};
+
+DropdownToggle.defaultProps = {
+  'aria-haspopup': true,
+  color: 'secondary',
+};
+
+DropdownToggle.contextTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+};
+
+export default DropdownToggle;
