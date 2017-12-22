d86b976e0e6e44d3da7fb1809056cfcbebb2587e
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2017-12-22 10:33:24 +0100

    build scripts

--- src/components/DropdownToggle.js
+++ src/components/DropdownToggle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Target } from 'react-popper';
-import { Button } from 'mdbreact';
+import Button from './Button';
 
 class DropdownToggle extends React.Component {
   constructor(props) {
